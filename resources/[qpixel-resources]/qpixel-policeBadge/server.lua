0b494274f734d05560e122674065f5d771ae2161
Gascanu <gascanumystery@gmail.com>
2022-12-09 19:10:51 +0200

    Update server.lua

--- resources/[qpixel-resources]/qpixel-policeBadge/server.lua
+++ resources/[qpixel-resources]/qpixel-policeBadge/server.lua
@@ -1,21 +1,16 @@
 RegisterServerEvent("qpixel-policeBadge:showBadge")
 AddEventHandler("qpixel-policeBadge:showBadge", function()
+  -- local src = source
   local src = source
-  local src = source
-  local user = exports['qpixel-base']:GetModule('GetPlayer')(src)
-  local cid = user["PlayerData"]["id"] 
+  local xPlayer = vRP.getUserId(src)
+  -- local cid = user["PlayerData"]["id"] 
 
-  local name = user["PlayerData"]["first_name"] .. " " .. user["PlayerData"]["last_name"]
-  exports.oxmysql:execute('SELECT * FROM characters WHERE id = ?', {cid}, function(result)
-    local imgLink = result[1].profilepic
-    exports.oxmysql:execute('SELECT * FROM character_jobwhitelist WHERE cid = ?', {cid}, function(result)
-      local callsign = result[1].callsign
-      if result[1].job == "police" or result[1].job == 'state' or result[1].job == 'sheriff' then
-        TriggerClientEvent('icemallow-badge:badgeanim', src)
-        TriggerClientEvent('icemallow-badge:open', -1, name, imgLink, callsign, src)
-      else
-        TriggerClientEvent("DoLongHudText", src, 'This is not your badge',1)
-      end
-    end)
-  end)  
+  local name = Player(src).state.char_info.nume .. " " .. Player(src).state.char_info.prenume
+  local callsign = vRP.getMetaData(xPlayer)["callsign"]
+  if vRP.isUserInFaction(xPlayer,"Politie") then
+    TriggerClientEvent('icemallow-badge:badgeanim', src)
+    TriggerClientEvent('icemallow-badge:open', -1, name, callsign, src)
+  else
+    TriggerClientEvent("DoLongHudText", src, 'This is not your badge',1)
+  end
 end)
